c4d4ed14932a40000021bac74ba2816446e5bb55
Luciano Dante Cecere <ldante86@aol.com>
2016-11-09 16:48:52 -0800

    created html.sh

---
+++ scripts/html.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -
+
+# This script generates and html calendar and displays it in a browser.
+
+# Usage html.sh [date string]
+
+#browser=/usr/bin/yelp
+browser=/usr/bin/firefox
+#browser=/usr/bin/google-chrome
+
+# These browsers don't interpret CSS, so no bolding.
+#browser=w3m
+#browser=lynx
+
+calendar="/tmp/$(date +%m-%d-%Y).html"
+../lcal --html "$@" > "$calendar"
+"$browser" "$calendar"
+
+rm "$calendar"
