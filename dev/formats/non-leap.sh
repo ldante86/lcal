8b03a817c450117bcb3aee210924915d71a25695
Luciano Dante Cecere <ldante86@aol.com>
2016-11-23 01:15:31 -0800

    perpetual calendar in progress

---
+++ dev/formats/non-leap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -
+
+_is_leap()
+{
+        local y=$1
+        if [ $((y % 4)) -eq 0 ] && 
+           [ $((y % 100)) -ne 0 ] || 
+           [ $((y % 400)) -eq 0 ]; then
+                return 1
+        fi
+}
+
+days=(Sunday Monday Tuesday Wednesday Thursday Friday Saturday)
+
+n=0
+for i in {1900..1960}
+do
+	_is_leap $i || continue
+
+	if [ "$(lcal -d 1 1 $i)" = "${days[n]}" ]; then
+		echo $i
+		((n++))
+	fi
+done
